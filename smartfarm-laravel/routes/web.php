fb2d2bc1f9bebb6e01507a38c42efab406eb69ac
ayu-karliwana <ayu.karliwana@student.undiksha.ac.id>
2026-06-25 12:11:10 +0800

    Update web.php

--- smartfarm-laravel/routes/web.php
+++ smartfarm-laravel/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\LandPredictionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [LandPredictionController::class, 'dashboard'])->name('dashboard');
+    Route::resource('predictions', LandPredictionController::class);
 });
